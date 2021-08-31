825d69a8812c8cbaa844e21f3025cb92b6e9b4c2
Abdi <abditorres71@gmail.com>
2021-08-30 19:29:05 -0700

    push

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,26 @@
 
 import './App.css';
 import  Logo  from './utils/logo.png';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Dogs from "./pages/Home"
+import Cats from "./pages/About"
+import Sheeps from "./pages/Contact"
+import Goats from "./pages/Shop"
 function App() {
   return (
     <div className="App">
       
       <img src={Logo} alt="Logo" />
-      
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Dogs} />
+        <Route path='/cats' component={Cats} />
+        <Route path='/sheeps' component={Sheeps} />
+        <Route path='/goats' component={Goats} />
+      </Switch>
+    </Router>
     </div>
   );
 }
