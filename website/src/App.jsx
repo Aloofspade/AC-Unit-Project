4bf7e43447373b90d753c9c227e5c47671129f41
Abdi Torres Romero <Atorre278@west-mec.local>
2021-08-30 17:51:54 -0700

    push

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,9 +1,11 @@
 
 import './App.css';
-
+import  Logo  from './utils/logo.png';
 function App() {
   return (
     <div className="App">
+      
+      <img src={Logo} alt="Logo" />
       
     </div>
   );
