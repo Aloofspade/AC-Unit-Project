0c58267a62a1aea7e0892b37559a84a541f76c51
Abdi <abditorres71@gmail.com>
2021-09-06 14:47:10 -0700

    push

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 import React from "react"; 
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Navbar from "./pages/Navbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Shop from "./pages/Shop"
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
 import Footer from "./components/footer";
 function App() {
   return (
--- website/src/pages/About.jsx
+++ website/src/pages/About.jsx
@@ -3,25 +3,25 @@ import ceo from '../components/images/ceo.jpg'
 import artdirector from '../components/images/artdirector.jpg';
 import designer from '../components/images/designer.jpg';
 
-const Sheeps = () =>{
+const About = () =>{
   return (
     <div>
       <div className="about">
   <div className="about-section">
-    <h1>About Us Page</h1>
-    <p>Some text about who we are and what we do.</p>
-    <p>Resize the browser window to see that this page is responsive by the way.</p>
+    <h1 className="pabout">About Us Page</h1>
+    <p className="pabout">Some text about who we are and what we do.</p>
+    <p className="pabout">Resize the browser window to see that this page is responsive by the way.</p>
   </div>
-  <h2 style={{"-webkit-text-align":"center","text-align":"center"}}>Our Team</h2>
+  <h2 style={{"-webkit-text-align":"center","text-align":"center", "fontFamily":"italic"}}>Our Team</h2>
   <div className="row">
-    <div className="column">
+    <div className="column1">
       <div className="card">
         <img src={ceo} alt="Jane" style={{"width":"100%", "height":"100%"}} />
-        <div className="container">
+        <div className="container3">
           <h2>John Doe</h2>
-          <p className="title">CEO &amp; Founder</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>john@example.com</p>
+          <p className="title1">CEO &amp; Founder</p>
+          <p className="pabout">Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p className="pabout">john@example.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
@@ -29,11 +29,11 @@ const Sheeps = () =>{
     <div className="column">
       <div className="card">
         <img src={artdirector} alt="Mike" style={{"width":"100%", "height":"100%"}} />
-        <div className="container">
+        <div className="container3">
           <h2>Mike Ross</h2>
-          <p className="title">Art Director</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>mike@example.com</p>
+          <p className="title1">Art Director</p>
+          <p className="pabout">Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p className="pabout">mike@example.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
@@ -41,11 +41,11 @@ const Sheeps = () =>{
     <div className="column">
       <div className="card">
         <img src={designer} alt="John" style={{"width":"100%", "height":"100%"}} />
-        <div className="container">
+        <div className="container3">
           <h2>Jane Madan</h2>
-          <p className="title">Designer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>jane@example.com</p>
+          <p className="title1">Designer</p>
+          <p className="pabout">Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p className="pabout">jane@example.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
@@ -55,4 +55,4 @@ const Sheeps = () =>{
     </div>
   );
 }
-export default Sheeps;
+export default About;
